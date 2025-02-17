850f5ab935a0e0e5a5b3bdce9cad001e9ab57223
Subrat Ghosh <126956177+subrat0@users.noreply.github.com>
2025-02-18 00:47:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,6 @@ document.getElementById('contact-btn').addEventListener('click', function() {
 
 
 document.getElementById('contact-form').addEventListener('submit', function(event) {
-    
-
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const message = document.getElementById('message').value;
